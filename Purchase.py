7c9059ab7ceba95b3be647836fce28291df53827
Kevin Nam <kevin.nam@mail.mcgill.ca>
2014-12-01 16:19:42 -0500

    Updated Inventory and Purchase.py and Catalogue

--- Purchase.py
+++ Purchase.py
@@ -2,7 +2,7 @@
 import cgi,cgitb,csv
 
 form = cgi.FieldStorage()
-
+	
 #Setting up Inventory 
 f = open("Inventory.csv","r")
 cr = csv.reader(f)
@@ -44,55 +44,69 @@ print"""
 <!--END HEADER-->
 """
 print "<body><center><br>"
-print "<h1>You ordered the following: </h1>"
 
-if form.getvalue('beanie'):
-	if(int(item[0][1])):
-		item[0][1] = int(item[0][1]) - 1
-		print "<p>Charcoal Black UG Beanie - $10</p>"
-		sum += 10
-	else:
-		print "<p>Charcoal Black UG Beanie - Sold out!</p>"
-		
-if form.getvalue('crewneck'):
-	if(int(item[1][1])):
-		item[1][1] = int(item[1][1]) - 1
-		print "<p>Heather Black UG Crewneck - $20</p>"
-		sum += 20
-	else:
-		print"<p>Heather Black UG Crewneck - Sold out!</p>"
-if form.getvalue('shirt'):
-	if(int(item[2][1])):
-		item[2][1] = int(item[2][1]) - 1
-		print "<p>Black UG T-Shirt - $15</p>"
-		sum += 15
-	else:
-		print"<p>Black UG T-Shirt - Sold out!</p>"
-if form.getvalue('snapback'):
-	if(int(item[3][1])):
-		item[3][1] = int(item[3][1]) - 1 
-		print "<p>Black UG Snapback - $12</p>"
-		sum += 12
-	else:
-		print "<p>Black UG Snapback - Sold out!</p>"
-if form.getvalue('baseball'):
-	if(int(item[4][1])):
-		item[4][1] = int(item[4][1]) - 1
-		print "<p>Black UG Baseball Tee - $15</p>"
-		sum += 15
-	else:
-		print"<p>Black UG Baseball Tee - Sold out!</p>"
-if form.getvalue('pants'):
-	if(int(item[5][1])):
-		item[5][1] = int(item[5][1]) - 1
-		print "<p>Black UG Sports Pants - $30</p>"
-		sum += 30
-	else:
-		print"<p>Black UG Sports Pants - Sold Out!</p>"
-print "<br><h1> Total cost: <b>$ %.2f</b></h1>" %(sum)
-print '<a href="index.html">Return to homepage</a>'
+#CHECKS IF USER IS LOGGED IN:
+user = form.getvalue("user")
+if user != "null":
+	print "<h1>You must be logged in to order</h1>"
+	print '<a href="login.html">Click here to login</a>'
+	print "</center></body>"
+else:	
+	#QUANTITY USED
+	beaniex = form.getvalue('beaniex')
+	crewneckx = form.getvalue('crewneckx')
+	shirtx = form.getvalue('shirtx')
+	snapbackx = form.getvalue('snapbackx')
+	baseballx = form.getvalue('baseballx')
+	pantsx = form.getvalue('pantsx')
+	print "<h1>You ordered the following: </h1>"
+	if form.getvalue('beanie'):
+		if(int(item[0][1])>=int(beaniex)):
+			item[0][1] = int(item[0][1]) - int(beaniex)
+			print "<p>%d x Charcoal Black UG Beanie - $%d</p>" %(int(beaniex),10*int(beaniex))
+			sum += 10*int(beaniex)
+		else:
+			print "<p>Charcoal Black UG Beanie - Insufficient Quantity</p>"
+			
+	if form.getvalue('crewneck'):
+		if(int(item[1][1])>=int(crewneckx)):
+			item[1][1] = int(item[1][1]) - int(crewneckx)
+			print "<p>%d x Heather Black UG Crewneck - $%d</p>" %(int(crewneckx),20*int(crewneckx))
+			sum += 20*int(crewneckx)
+		else:
+			print"<p>Heather Black UG Crewneck - Insufficient Quantity</p>"
+	if form.getvalue('shirt'):
+		if(int(item[2][1])>=int(shirtx)):
+			item[2][1] = int(item[2][1]) - int(shirtx)
+			print "<p>%d x Black UG T-Shirt - $%d</p>" %(int(shirtx),15*int(shirtx))
+			sum += 15*int(shirtx)
+		else:
+			print"<p>Black UG T-Shirt - Insufficient Quantity</p>"
+	if form.getvalue('snapback'):
+		if(int(item[3][1])>=int(snapbackx)):
+			item[3][1] = int(item[3][1]) - int(snapbackx)
+			print "<p>%d x Black UG Snapback - $%d</p>" %(int(snapbackx),12*int(snapbackx))
+			sum += 12*int(snapbackx)
+		else:
+			print "<p>Black UG Snapback - Insufficient Quantity</p>"
+	if form.getvalue('baseball'):
+		if(int(item[4][1])>=int(baseballx)):
+			item[4][1] = int(item[4][1]) -int(baseballx)
+			print "<p>%d x Black UG Baseball Tee - $%d</p>" %(int(baseballx),15*int(baseballx))
+			sum += 15*int(baseballx)
+		else:
+			print"<p>Black UG Baseball Tee - Insufficient Quantity!</p>"
+	if form.getvalue('pants'):
+		if(int(item[5][1])>=int(pantsx)):
+			item[5][1] = int(item[5][1]) - int(pantsx)
+			print "<p>%d x Black UG Sports Pants - $%d</p>" %(int(pantsx),30*int(pantsx))
+			sum += 30*int(pantsx)
+		else:
+			print"<p>Black UG Sports Pants - Insufficient Quantity</p>"
+	print "<br><h1> Total cost: <b>$ %.2f</b></h1>" %(sum)
+	print '<a href="index.html">Return to homepage</a>'
 
-print "</center></body>"
+	print "</center></body>"
 
 print """
 <!-- START FOOTER -->
